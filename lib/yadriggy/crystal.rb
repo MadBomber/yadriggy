64dc21a4b33a57a0c3d0d7f5b72b7852370df94a
Dewayne VanHoozer <dewayne@mars.local>
2018-07-12 10:22:51 -0500

    set the placeholders for both crystal and rust; hope I get some time to really explore this idea

---
+++ lib/yadriggy/crystal.rb
@@ -0,0 +1,9 @@
+# yadriggy/crystal.rb
+
+require_rel 'crystal/crystal'
+
+module Yadriggy
+  # Crystal language embedded in Ruby
+  module Crystal
+  end
+end
---
+++ lib/yadriggy/rust.rb
@@ -0,0 +1,9 @@
+# yadriggy/rust.rb
+
+require_rel 'rust/rust'
+
+module Yadriggy
+  # Rust language embedded in Ruby
+  module Rust
+  end
+end
